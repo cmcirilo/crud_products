dec8edc2ce9751935b64eb64f00cfee1b473e0de
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-02-10 16:53:12 -0300

    implementing read/list

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ForDirective } from './directives/for.directive';
 import { RedDirective } from './directives/red.directive';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 
 
@@ -34,7 +35,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     ProductCrudComponent,
     RedDirective,
     ForDirective,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
